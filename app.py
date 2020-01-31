80f1348afab39a91b858823cecdd9079d6a2ef1d
Sigrid Ledsaak <sigrid.ledsaak@gmail.com>
2020-01-31 11:59:51 +0100

    Added map

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
