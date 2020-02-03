fcb0a14c8ec029bc093113c60eed5da6af6a19ab
Sigrid Ledsaak <sigrid.ledsaak@gmail.com>
2020-02-03 10:43:56 +0100

    Updated sidebar to display content when clicked

--- static/sidebar.js
+++ static/sidebar.js
@@ -1,3 +1,19 @@
+
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle("active");
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    console.log(coll.length);
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
 }
---
+++ static/upload.js
@@ -0,0 +1,4 @@
+
+function uploadFile(){
+    return ""
+}
