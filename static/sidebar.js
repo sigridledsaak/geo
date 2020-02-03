f84f958dac9542fa8700527230e3838afb1906ed
Sigrid Ledsaak <sigrid.ledsaak@gmail.com>
2020-02-03 10:51:59 +0100

    Adding symbols

--- static/sidebar.js
+++ static/sidebar.js
@@ -3,7 +3,6 @@ function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle("active");
     var coll = document.getElementsByClassName("collapsible");
     var i;
-    console.log(coll.length);
 
     for (i = 0; i < coll.length; i++) {
       coll[i].addEventListener("click", function() {
