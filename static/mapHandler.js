c93135154a67ef9c0121f2cf79fdb34cde5bd587
Sigrid Ledsaak <sigrid.ledsaak@gmail.com>
2020-06-07 20:35:54 +0200

    Fix to migrate from classic styles

--- static/mapHandler.js
+++ static/mapHandler.js
@@ -5,8 +5,8 @@ var layers = {
     Light: L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'),
     Dark : L.mapbox.styleLayer('mapbox://styles/mapbox/dark-v10'),
     Satellite: L.mapbox.styleLayer('mapbox://styles/mapbox/satellite-v9'),
-
-
   };
+//Sets the default map to be the Custom one.
   layers.Custom.addTo(map);
+  //Adding the layer switcher to the map.
   L.control.layers(layers).addTo(map);
